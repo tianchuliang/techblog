590bf288e567e0694e47ef7202ce3b3c7787c388
Alex Liang <aliang@atd-us.com>
2019-08-18 21:09:54 -0400

    about to try some new tf stuff

--- pyTorch-NeuralStyleTransfer/helper.py
+++ pyTorch-NeuralStyleTransfer/helper.py
@@ -58,19 +58,15 @@ def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
                                content_layers,
                                style_layers):
     cnn = copy.deepcopy(cnn)
-
     # normalization module
     normalization = Normalization(normalization_mean, normalization_std).to(device)
-
     # just in order to have an iterable access to or list of content/syle
     # losses
     content_losses = []
     style_losses = []
-
     # assuming that cnn is a nn.Sequential, so we make a new nn.Sequential
     # to put in modules that are supposed to be activated sequentially
     model = nn.Sequential(normalization)
-
     i = 0  # increment every time we see a conv
     for layer in cnn.children():
         if isinstance(layer, nn.Conv2d):
@@ -88,30 +84,24 @@ def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
             name = 'bn_{}'.format(i)
         else:
             raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
-
         model.add_module(name, layer)
-
         if name in content_layers:
             # add content loss:
             target = model(content_img).detach()
             content_loss = ContentLoss(target)
             model.add_module("content_loss_{}".format(i), content_loss)
             content_losses.append(content_loss)
-
         if name in style_layers:
             # add style loss:
             target_feature = model(style_img).detach()
             style_loss = StyleLoss(target_feature)
             model.add_module("style_loss_{}".format(i), style_loss)
             style_losses.append(style_loss)
-
     # now we trim off the layers after the last content and style losses
     for i in range(len(model) - 1, -1, -1):
         if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
             break
-
     model = model[:(i + 1)]
-
     return model, style_losses, content_losses
 
 def get_input_optimizer(input_img):
